0725e6e12ee03e6df60cfe669f2222852db04294
KarnatiMahalaxmi <143473144+KarnatiMahalaxmi@users.noreply.github.com>
2023-10-02 22:09:04 +0530

    Add files via upload

---
+++ days to years.cpp
@@ -0,0 +1,13 @@
+/*to convert days into years*/
+#include<stdio.h>
+int main ()
+{
+	int a;
+	float y;
+	printf("enter the number of days");
+	scanf("%d",&a);
+	y=a/365;
+	printf("the number of years");
+	scanf("%f",&y);
+	return 0;
+}
