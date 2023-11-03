0d5eab632edf16a022d4f4431c53a9cfbed7cc61
KarnatiMahalaxmi <143473144+KarnatiMahalaxmi@users.noreply.github.com>
2023-11-03 20:59:35 +0530

    Add files via upload

---
+++ cube.cpp
@@ -0,0 +1,12 @@
+/* to find cube of a number*/
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int a,b;
+printf("given number=");
+scanf("%d",&a);
+b=pow(a,3);
+printf("the cube of a is %d",b);
+return 0;
+}
